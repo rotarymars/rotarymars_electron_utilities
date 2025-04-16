22de3c24eb8adbf58a293df3fd2774d3fc63ec3b
rotarymars <s.goto2050@gmail.com>
2025-04-16 22:13:50 +0900

    main.js: Created main.js for electron initialization

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Keep a global reference of the window object to prevent it from being garbage collected
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Load the index.html file
+  mainWindow.loadFile('index.html');
+
+  // Open DevTools in development mode
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
+
+  // Emitted when the window is closed
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished initialization
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  // On macOS it's common to keep the app running until the user explicitly quits
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window when the dock icon is clicked
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
